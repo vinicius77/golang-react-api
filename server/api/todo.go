18270d36f0f1203834d8479815ae1997dc1f2e3f
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2023-05-17 22:55:35 +0300

    feat(api): CRUD functions declaration

--- server/api/todo.go
+++ server/api/todo.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/vinicius77/golang-react-api/structs"
 )
 
+var todos = []structs.Todo{}
+
 func CreateTodo(ctx *fiber.Ctx) error {
-	todos := []structs.Todo{}
 	todo := &structs.Todo{}
 
 	if error := ctx.BodyParser(todo); error != nil {
@@ -18,4 +21,48 @@ func CreateTodo(ctx *fiber.Ctx) error {
 
 	return ctx.JSON(todos)
 
+}
+
+func ListTodo(ctx *fiber.Ctx) error {
+	return ctx.JSON(todos)
+}
+
+func UpdateTodo(ctx *fiber.Ctx) error {
+	id, error := ctx.ParamsInt("id")
+
+	if error != nil {
+		return ctx.Status(400).SendString("Invalid id")
+	}
+
+	for index, todo := range todos {
+		if todo.ID == id {
+			todos[index].Done = true
+			break
+		}
+	}
+
+	return ctx.JSON(todos)
+}
+
+func DeleteTodo(ctx *fiber.Ctx) error {
+	id, error := ctx.ParamsInt("id")
+	errorMessage := fmt.Sprintf("Todo with id %d was not found", id)
+	doesIdExist := false
+
+	if error != nil {
+		return ctx.Status(400).SendString("Bad Request")
+	}
+
+	for index, todo := range todos {
+		if todo.ID == id {
+			doesIdExist = true
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+
+	if doesIdExist {
+		return ctx.Status(204).SendString("No Content")
+	}
+	return ctx.Status(404).SendString(errorMessage)
 }
