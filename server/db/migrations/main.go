3b94ec65451c3596c4615b6a20240d26c06ef3a0
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-07 18:48:07 +0300

    feat(migrations): the actual migration files, mySQL storage instance

---
+++ server/db/migrations/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	mysqlCfg "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/vinicius77/golang-react-api/db"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysqlCfg.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "firebase-authentication-app",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"mysql",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+}
