e4bfdd66327ea9d5723839dce222eeb960753376
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-07 18:50:00 +0300

    feat(model): maybe will be ditched after giving up in using goose for migration

---
+++ server/domain/model/user.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint       `json:"id"`
+	UUID      string     `json:"uuid"`
+	Email     string     `json:"email"`
+	CreatedAt *time.Time `json:"createdAt"`
+	UpdatedAt *time.Time `json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
+}
+
+// TableName gets table name
+func (u *User) TableName() string {
+	return "users"
+}
