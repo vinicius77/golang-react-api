4a0bf3fe0b328b7d77b96048c722bca99c55c8e2
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-08 22:20:10 +0300

    feat(main.go): instantiate DB, implement create user post request

--- server/main.go
+++ server/main.go
@@ -5,9 +5,26 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/vinicius77/golang-react-api/api"
+	"github.com/vinicius77/golang-react-api/db"
+
+	mysqlCfg "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+
+	db, err := db.NewMySQLStorage(mysqlCfg.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "firebase-authentication-app",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	logFatal(err)
+	defer db.Close()
+
 	app := fiber.New()
 
 	app.Post("/v1/todos", api.CreateTodo)
@@ -15,5 +32,15 @@ func main() {
 	app.Get("/v1/todos", api.ListTodo)
 	app.Delete("/v1/todos/:id", api.DeleteTodo)
 
-	log.Fatal(app.Listen(":4000"))
+	app.Post("/v1/users", func(c *fiber.Ctx) error {
+		return api.CreateUser(c, db)
+	})
+
+	logFatal(app.Listen(":4000"))
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
