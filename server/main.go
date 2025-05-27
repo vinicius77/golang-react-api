0c249c8035b806f8b1ccaa1cfe5e8f6fc1dfa3d4
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-27 21:33:43 +0300

    feat(firebase-init): initiates firebase, setup cors and protected routes

--- server/main.go
+++ server/main.go
@@ -4,13 +4,27 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/vinicius77/golang-react-api/api"
+	"github.com/vinicius77/golang-react-api/api/middleware"
+	"github.com/vinicius77/golang-react-api/config"
 	"github.com/vinicius77/golang-react-api/db"
+	"github.com/vinicius77/golang-react-api/handler"
+	"github.com/vinicius77/golang-react-api/service"
 
 	mysqlCfg "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+
+	firebaseApp, err := config.InitFirebaseApp()
+
+	if err != nil {
+		log.Fatal("error initializing firebase", err)
+	}
+	authService := service.NewAuthService(firebaseApp)
+	authHandler := handler.NewAuthHandler(authService)
+	authMiddleware := middleware.NewAuthMiddleware(authService)
 
 	db, err := db.NewMySQLStorage(mysqlCfg.Config{
 		User:                 "root",
@@ -35,6 +49,17 @@ func main() {
 	app.Post("/v1/users", func(c *fiber.Ctx) error {
 		return api.CreateUser(c, db)
 	})
+
+	// Protected routes with a subrouter
+	protectedRouter := app.Group("/api", cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,OPTIONS",
+		AllowHeaders:     "Accept,Content-Type,X-Request-With,Authorization",
+		ExposeHeaders:    "Content-Length,Content-Type",
+		AllowCredentials: true,
+		MaxAge:           300,
+	}), authMiddleware.Authentication())
+	protectedRouter.Get("/profile", authHandler.GetProfile)
 
 	logFatal(app.Listen(":4000"))
 }
