8da59231e16f176ecf70da17522d00d0ca50111f
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-31 09:46:14 +0300

    feat(react-router): initial implementation of routing on App.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,29 @@
-import SignUpWithEmail from "./SignUpWithEmail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from "@/routes/ProtectedRoute";
+import PublicRoute from "@/routes/PublicRoute";
+
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 
 import "./App.css";
 
 function App() {
-  return <SignUpWithEmail />;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
