e65420a11e44079b8d4e230a4c57d71ee4bf2595
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-31 09:52:23 +0300

    feat(pages): initial barebone pages (Home, Register, SignIn)

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,12 @@
+import SignUpWithEmail from "@/components/commons/SignUpWithEmail";
+
+const Register = () => {
+  return (
+    <div>
+      Register
+      <SignUpWithEmail />
+    </div>
+  );
+};
+
+export default Register;
