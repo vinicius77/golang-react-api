156685d4fe2e8b6068624a5f515483715bc4eee3
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-05-31 09:51:02 +0300

    feat(routes): barebones for both public and protected routes

---
+++ client/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "@/contexts/AuthContext";
+
+const ProtectedRoute = () => {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>;
+  if (!authContext?.state?.user) return <Navigate to="/login" replace />;
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
---
+++ client/src/routes/PublicRoute.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "@/contexts/AuthContext";
+
+import useNavigateUser from "@/hooks/useNavigateUser";
+
+const PublicRoute = () => {
+  const authContext = useContext(AuthContext);
+  useNavigateUser();
+
+  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>;
+  if (authContext?.state?.user) return <Navigate to="/" replace />;
+
+  return <Outlet />;
+};
+
+export default PublicRoute;
