e98b1cddeab7e8a6a40ff86a02c1acdea3854ace
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-06-03 21:40:35 +0300

    feat(top-nav-bar): entrypoint component for top nav bar

---
+++ client/src/components/commons/TopNavBar/TopNavbar.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react"
+
+import Navbar from "@/components/commons/TopNavBar/Navbar"
+import NavItem from "@/components/commons/TopNavBar/NavItem"
+import DropdownMenu from "@/components/commons/TopNavBar/DropdownMenu"
+
+import BellIcon from "@/icons/bell.svg?react"
+import Plus from "@/icons/plus.svg?react"
+import Caret from "@/icons/caret.svg?react"
+
+import { AuthContext } from "@/contexts/AuthContext"
+
+import "./Topbar.css"
+
+const TopNavbar = () => {
+  const authContext = useContext(AuthContext)
+  if (!authContext?.state?.user) return null
+
+  return (
+    <Navbar>
+      <NavItem icon={<Plus />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<Caret />}>
+        <DropdownMenu />
+      </NavItem>
+    </Navbar>
+  )
+}
+
+export default TopNavbar
