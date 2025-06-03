f3f2499bb1fda2e284b9aea534fe8a64b2824277
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-06-03 21:30:47 +0300

    improve(config): svg config related

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "node:url";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: [
       {
