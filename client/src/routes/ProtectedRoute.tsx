1e00c85dfb5ffff716ba1086bab55421cc403c72
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-06-04 18:29:19 +0300

    improve(format-file): prettier does its thing

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,14 +1,14 @@
-import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom"
+import { AuthContext } from "@/contexts/auth-context"
 
 const ProtectedRoute = () => {
-  const authContext = useContext(AuthContext);
+  const authContext = useContext(AuthContext)
 
-  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>;
-  if (!authContext?.state?.user) return <Navigate to="/login" replace />;
+  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>
+  if (!authContext?.state?.user) return <Navigate to="/login" replace />
 
-  return <Outlet />;
-};
+  return <Outlet />
+}
 
-export default ProtectedRoute;
+export default ProtectedRoute
--- client/src/routes/PublicRoute.tsx
+++ client/src/routes/PublicRoute.tsx
@@ -1,17 +1,17 @@
-import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom"
+import { AuthContext } from "@/contexts/auth-context"
 
-import useNavigateUser from "@/hooks/useNavigateUser";
+import useNavigateUser from "@/hooks/useNavigateUser"
 
 const PublicRoute = () => {
-  const authContext = useContext(AuthContext);
-  useNavigateUser();
+  const authContext = useContext(AuthContext)
+  useNavigateUser()
 
-  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>;
-  if (authContext?.state?.user) return <Navigate to="/" replace />;
+  if (!authContext?.state?.authIsReady) return <div>Loading ...</div>
+  if (authContext?.state?.user) return <Navigate to="/" replace />
 
-  return <Outlet />;
-};
+  return <Outlet />
+}
 
-export default PublicRoute;
+export default PublicRoute
