7742f2b9fa24165617b14d4c4294e276edff8273
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-06-04 18:30:21 +0300

    improve(main.tsx): wrap application with toast notification provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import AuthContextProvider from "./contexts/AuthContext.tsx";
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import App from "@/App.tsx"
+import AuthContextProvider from "@/contexts/auth-context"
+import ToastProvider from "@/providers/ToastProvider"
+
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </AuthContextProvider>
   </React.StrictMode>
-);
+)
