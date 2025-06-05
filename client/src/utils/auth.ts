d006217b23872d7171eac3580286ec41c9487e24
Vinícius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2025-06-05 19:01:25 +0300

    feat(utils): auth for signInWithFirebaseProvider

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,30 @@
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  UserCredential,
+} from "firebase/auth"
+
+import { auth } from "@/utils/firebase"
+import { ProviderOpts } from "@/components/commons/AuthCard/AuthCard"
+
+interface SignInResult {
+  result: UserCredential | null
+  error: Error | null
+}
+
+export const signInWithProvider = async (
+  provider: ProviderOpts
+): Promise<SignInResult> => {
+  const providerInstance =
+    provider === "Google" ? new GoogleAuthProvider() : new GithubAuthProvider()
+
+  try {
+    const result = await signInWithPopup(auth, providerInstance)
+    console.log({ result })
+    return { result, error: null }
+  } catch (err) {
+    const error = err instanceof Error ? err : new Error("Unknown error")
+    return { result: null, error }
+  }
+}
